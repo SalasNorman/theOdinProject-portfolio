a9a5c482f5707098ad0539e307c07dbf78ac84fb
SalasNorman <salasnorman015@gmail.com>
2026-01-06 10:08:21 +0800

    add features and refactor code

--- foundation-course/etch-a-sketch/script.js
+++ foundation-course/etch-a-sketch/script.js
@@ -1,42 +1,83 @@
 const container = document.querySelector(".container");
 const gridSizeLabel = document.querySelector("#grid-size-display");
 const gridSizeInput = document.querySelector("#grid-size");
+let colorPick = document.querySelector("#color-picker");
+let brushMode = document.querySelector("#brush-mode");
+let eraserMode = document.querySelector("#eraser-mode");
+let clearCanvas = document.querySelector("#clear-canvas");
 
 let defaultGridSize = 16;
+colorPick.value = `hsla(0, 0%, 0%, 0.10)`;
 gridSizeLabel.textContent = `${defaultGridSize} x ${defaultGridSize}`;
 
-// paint
-container.addEventListener("mouseover", (event) => {
-  let target = event.target;
-  if (target.className !== "container") {
-    target.classList.add("highlight");
-  }
-});
-
 // grid slider
-function displayGridSize(){
-gridSizeInput.addEventListener("input", (event) => {
-  defaultGridSize = event.target.value;
-  gridSizeLabel.textContent = `${defaultGridSize} x ${defaultGridSize}`;
-  // remove previous grid
+function displayGridSize() {
+  gridSizeInput.addEventListener("input", (event) => {
+    defaultGridSize = event.target.value;
+    gridSizeLabel.textContent = `${defaultGridSize} x ${defaultGridSize}`;
+    resetGrid();
+  });
+}
+
+function resetGrid() {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
   generateGrid();
-});
 }
 
 function generateGrid() {
   let gridSize = defaultGridSize * defaultGridSize;
   for (let i = 1; i <= gridSize; i++) {
     const grid = document.createElement("div");
-    let hw = 352 / defaultGridSize - 2;
+    let hw = 350 / defaultGridSize;
     grid.classList.add("box");
     grid.style.height = `${hw}px`;
     grid.style.width = `${hw}px`;
     container.appendChild(grid);
   }
 }
+
+// brushing
+container.addEventListener("mouseover", (event) => {
+  let box = event.target;
+  if (
+    box.className !== "container" &&
+    colorPick.value !== "" &&
+    brushMode.value === "ON"
+  ) {
+    box.style.backgroundColor = `${colorPick.value}`;
+  } else if (eraserMode.value === "ON") {
+    box.style.backgroundColor = ``;
+  }
+});
+
+brushMode.addEventListener("click", (event) => {
+  toggle(brushMode, eraserMode);
+});
+
+eraserMode.addEventListener("click", (event) => {
+  toggle(eraserMode, brushMode);
+});
+
+function toggle(btn1, btn2) {
+  if (btn1.value === "OFF") {
+    btn1.value = "ON";
+    btn2.value = "OFF";
+  } else if (btn1.value === "ON") {
+    btn1.value = "OFF";
+    btn1.value = "ON";
+  }
+}
+
+clearCanvas.addEventListener("click", (event) => {
+  resetGrid();
+});
+
+test.addEventListener("click", (event) => {
+  console.log(brushMode.value);
+  console.log(colorPick.value);
+});
 
 displayGridSize();
 generateGrid();
