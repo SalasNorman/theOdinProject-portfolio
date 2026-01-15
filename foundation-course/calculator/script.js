ed050dc04d040450303bea410ec43012928f2ad3
SalasNorman <salasnorman015@gmail.com>
2026-01-15 15:20:01 +0800

    refactor code

--- foundation-course/calculator/script.js
+++ foundation-course/calculator/script.js
@@ -1,57 +1,54 @@
 const input = document.querySelector("#input");
 const container = document.querySelector("#container");
 
-let currentInput = "";
-let input1 = "";
-let input2 = "";
-let operator = "";
-let nextInput = false;
-
 container.addEventListener("click", (event) => {
   if (
     event.target.id !== "container" &&
     event.target.className.includes("row") === false
   ) {
     currentInput = event.target.textContent;
-    // input.textContent = input1 + input2 + operator;
-    checkInput(currentInput);
+    checkCurrentInput(currentInput);
   }
 });
 
-function checkInput(currentInput) {
-  if (Number.isNaN(parseInt(currentInput))) {
-    if (input1 !== "") {
-      nextInput = true;
-      checkOperator(currentInput);
-    }
-  } else if (nextInput === true) {
-    input2 = input2.concat(currentInput);
-  } else {
-    input1 = input1.concat(currentInput);
-  }
-  console.log("1: " + input1);
-  console.log("2: " + input2);
-  console.log("op: " + operator);
-}
+let currentInput = "";
+let input1 = "";
+let input2 = "";
+let operator = "";
+let isNextInput = false;
+let result = "";
 
-function checkOperator(currentInput) {
-  if (currentInput === "+") {
-    operator = "+";
-  } else if (currentInput === "-") {
-    operator = "-";
-  } else if (currentInput === "x") {
-    operator = "*";
-  } else if (currentInput === "÷") {
-    operator = "/";
-  } else if (currentInput === "=") {
-    operate();
-  } else if (currentInput === "C") {
-    input1 = "";
-    input2 = "";
-    operator = "";
-    nextInput = false;
-    input.textContent = "";
+function checkCurrentInput(currentInput) {
+  let isSymbol = isNaN(parseInt(currentInput));
+
+  if (isSymbol === true) {
+    if (currentInput === "C") {
+      clearInput();
+    } else if (currentInput === "=") {
+      operate();
+      isNextInput = false;
+      operator = "";
+    } else {
+      // Arithmetic
+      if (input2 === "") {
+        // 1+_
+        operator = currentInput;
+        isNextInput = true;
+      } else if (input2 !== "") {
+        // 1+1
+        operate();
+        operator = currentInput;
+      }
+    }
+  } else if (isSymbol === false) {
+    // Numbers
+    if (isNextInput === false) {
+      input1 = input1.concat(currentInput);
+    } else if (isNextInput === true) {
+      input2 = input2.concat(currentInput);
+    }
   }
+  displayResult();
 }
 
 function operate() {
@@ -59,11 +56,22 @@ function operate() {
     input1 = Number(input1) + Number(input2);
   } else if (operator === "-") {
     input1 = Number(input1) - Number(input2);
-  } else if (operator === "*") {
+  } else if (operator === "x") {
     input1 = Number(input1) * Number(input2);
-  } else if (operator === "/") {
+  } else if (operator === "÷") {
     input1 = Number(input1) / Number(input2);
   }
-  input.textContent = input1;
+  input1 = String(input1);
   input2 = "";
+}
+
+function displayResult() {
+  input.textContent = `${input1}${operator}${input2}`;
+}
+
+function clearInput() {
+  input1 = "";
+  input2 = "";
+  operator = "";
+  isNextInput = false;
 }
