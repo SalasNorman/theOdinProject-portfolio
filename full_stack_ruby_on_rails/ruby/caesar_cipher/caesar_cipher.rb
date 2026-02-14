62929bfdf1249ba02631464eb61a0988daceafcc
SalasNorman <salasnorman015@gmail.com>
2026-02-14 15:10:56 +0800

    Add caesar_cipher and Gemfile + RuboCop config

--- full_stack_ruby_on_rails/ruby/caesar_cipher/caesar_cipher.rb
+++ full_stack_ruby_on_rails/ruby/caesar_cipher/caesar_cipher.rb
@@ -1,22 +1,24 @@
-def encryption_letter(letter, letter_position , shift)
-  encrypted_position = (letter.ord-letter_position+shift)%26
-  (encrypted_position+letter_position).chr
+def encryption_letter(letter, letter_position, shift)
+  encrypted_position = (letter.ord - letter_position + shift) % 26
+  (encrypted_position + letter_position).chr
 end
 
-def caesar_cipher(text,shift)
-  encrypted_text = ''
-  text.split("").each do |letter|
-    if /[[:upper:]]/.match?(letter)
-      encrypted_letter = encryption_letter(letter, 65, shift)
-    elsif /[[:lower:]]/.match?(letter)
-      encrypted_letter = encryption_letter(letter, 97, shift)
-    else
-      encrypted_letter = letter
-    end
-  encrypted_text << encrypted_letter
+# rubocop:disable Metrics/MethodLength
+def caesar_cipher(text, shift)
+  encrypted_text = ""
+  text.chars.each do |letter|
+    encrypted_letter = if /[[:upper:]]/.match?(letter)
+                         encryption_letter(letter, 65, shift)
+                       elsif /[[:lower:]]/.match?(letter)
+                         encryption_letter(letter, 97, shift)
+                       else
+                         letter
+                       end
+    encrypted_text << encrypted_letter
   end
   encrypted_text
 end
+# rubocop:enable Metrics/MethodLength
 
 # Encrypts the string "Hello World" using a Caesar cipher with a shift of 3
 puts caesar_cipher("Hello World", 3)
@@ -24,3 +26,6 @@ puts caesar_cipher("Hello World", 3)
 
 puts caesar_cipher("What a string!", 5)
 # Bmfy f xywnsl!
+
+puts caesar_cipher("Like you")
+puts "Hello"
